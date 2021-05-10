b32b01074ea1473819556724af24d552d83528f2
Max <62549548+Palaman1@users.noreply.github.com>
2021-05-10 14:32:44 +0300

    Update main.js
    add new code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(function(){
   var mixer = mixitup('.gallery__content');
   
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+  
   $('.slider-blog__inner').slick({
     dots: true,
     
